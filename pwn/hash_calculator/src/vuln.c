4f651daf7d8492c9334c61f3095ab0d82acbca05
y011d4 <yoiida333@gmail.com>
2022-03-31 09:19:28 +0900

    pwn

---
+++ pwn/hash_calculator/src/vuln.c
@@ -0,0 +1,40 @@
+#include <openssl/sha.h>
+#include <stdio.h>
+#include <string.h>
+
+void print_hash(char *s) {
+  unsigned char digest[SHA256_DIGEST_LENGTH];
+  SHA256_CTX sha_ctx;
+  SHA256_Init(&sha_ctx);
+  SHA256_Update(&sha_ctx, s, strlen(s));
+  SHA256_Final(digest, &sha_ctx);
+  for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+    printf("%x", digest[i]);
+  }
+  puts("");
+}
+
+int main() {
+  setvbuf(stdin, NULL, _IONBF, 0);
+  setvbuf(stdout, NULL, _IONBF, 0);
+  char input[0x100];
+  char content[0x100];
+  while (1) {
+    scanf("%255s", input);
+    if (strcmp(input, "exit") == 0) {
+      return 0;
+    }
+    printf(input);
+    puts("");
+    FILE *fp = fopen(input, "r");
+    if (fp == NULL) {
+      printf("File cannot be opened.");
+      puts("");
+      continue;
+    }
+    memset(content, 0, 0x100);
+    fread(&content, 0xff, 1, fp);
+    fclose(fp);
+    print_hash(content);
+  }
+}
